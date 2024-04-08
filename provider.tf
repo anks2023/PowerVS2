3a1a8809c89b4dd65654ee720cd5f5152de12e84
Ankur Sharma <138403850+anks2023@users.noreply.github.com>
2024-04-08 17:28:52 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,6 @@ terraform {
       source = "IBM-Cloud/ibm"
       ibmcloud_api_key = "owuTk68OtbpRxgOKLGEqrx2PTY-W1gpMHXm1Ps9y3oW5"
       region           = "eu-gb" 
-      version          = ">=1.49.0"
     }
   }
 }
