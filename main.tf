ec7669b54745e2966e9ceff30f0f2556f60ed1b9
Ankur Sharma <138403850+anks2023@users.noreply.github.com>
2024-04-15 18:52:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "ibm_pi_instance" "test-instance" {
     pi_processors         = "0.25"
     pi_instance_name      = "test-vm"
     pi_proc_type          = "shared"
-    pi_image_id           = "4c71bfb1-1f09-4816-aa14-298e882ccccd"
+    pi_image_id           = "79c3bb4f-6e86-4dea-a2fd-bd94365a3a80"
     pi_key_pair_name      = key1
     pi_sys_type           = "s922"
-    pi_cloud_instance_id  = "4c71bfb1-1f09-4816-aa14-298e882ccccd"
+    pi_cloud_instance_id  = "307928bf-b47d-4492-8d9a-20eb2b879087"
     pi_pin_policy         = "none"
     pi_network {
       network_id = data.ibm_pi_public_network.dsnetwork.id
