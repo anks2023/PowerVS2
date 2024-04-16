787b3559e1c912c0e3c7573c1215a42b426a566e
Ankur Sharma <138403850+anks2023@users.noreply.github.com>
2024-04-16 19:21:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-data "ibm_pi_image" "power_images" {
-    pi_image_name        = "7300-02-01"
-    pi_cloud_instance_id  = "307928bf-b47d-4492-8d9a-20eb2b879087"
+//data "ibm_pi_image" "power_images" {
+   // pi_image_name        = "7300-02-01"
+   // pi_image_id           = "79c3bb4f-6e86-4dea-a2fd-bd94365a3a80"
+    //pi_cloud_instance_id  = "307928bf-b47d-4492-8d9a-20eb2b879087"
+//}
+resource "ibm_pi_image" "testacc_image  "{
+  pi_image_name       = "7200-05-07"
+  pi_image_id         = "00b393a9-c627-4550-b42e-b862a19c344f"
+  pi_cloud_instance_id = "307928bf-b47d-4492-8d9a-20eb2b879087"
 }
 resource "ibm_pi_instance" "testvm1" {
     pi_memory             = "2"
@@ -10,7 +16,7 @@ resource "ibm_pi_instance" "testvm1" {
     pi_key_pair_name      = "key1"
     pi_sys_type           = "s922"
     pi_cloud_instance_id  = "307928bf-b47d-4492-8d9a-20eb2b879087"
-    pi_image_id           = "79c3bb4f-6e86-4dea-a2fd-bd94365a3a80"
+    pi_image_id           = resource.ibm_pi_image.testacc_image.pi_image_id
     pi_storage_type       = "tier3"
     pi_pin_policy         = "none"
     pi_network {
