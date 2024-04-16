92671fa4079231aed6d58de1e97740e05d1e18ec
Ankur Sharma <138403850+anks2023@users.noreply.github.com>
2024-04-16 18:41:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "ibm_pi_instance" "testvm1" {
     pi_key_pair_name      = "key1"
     pi_sys_type           = "s922"
     pi_cloud_instance_id  = "307928bf-b47d-4492-8d9a-20eb2b879087"
+    pi_image_id           = "79c3bb4f-6e86-4dea-a2fd-bd94365a3a80"
     pi_storage_type       = "tier3"
     pi_pin_policy         = "none"
     pi_network {
